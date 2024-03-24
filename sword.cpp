b2e06d1503dde5750f6e8aee788eeca545fd8ba6
Тимур Миндубаев <mindubaev.timurka@gmail.com>
2024-03-24 20:42:51 +0300

    Добавьте файлы проекта.

---
+++ sword.cpp
@@ -0,0 +1,56 @@
+﻿#include <windows.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string compress(const string& str)
+{
+    string word;
+    bool check = true;
+    bool check2 = true;
+
+    for (char symb : str)
+    {
+        if (check2) {
+            if (symb != ' ')
+            {
+                check2 = false;
+            }
+            else
+            {
+                word += ' ';
+                continue;
+            }
+        }
+        if (symb == ' ')
+        {
+            if (!check)
+            {
+                word += ' ';
+                check = true;
+            }
+        }
+        else
+        {
+            word += symb;
+            check = false;
+        }
+    }
+    return word;
+}
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    string str = "";
+    cout << "Программа для сжатия строк\n";
+    while (str != "0")
+    {
+        cout << "Введите строку для сжатия(или 0 для выхода)\n";
+        getline(cin, str);
+        if (str != "0") cout << "\nСжатая строка:" << compress(str) << endl;
+    }
+
+}
